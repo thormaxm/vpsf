a5d8e912219bb0d1e1fb484c3d2808876b5d2721
thormaxm <116080524+thormaxm@users.noreply.github.com>
2022-11-11 18:05:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'tnlink.in')
 URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '8a67ddab1eee04927475f39f714e01211aea76ed')
 
      # Auto Delete For Group Message (Self Delete) #
-SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 200))
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 80))
 SELF_DELETE = environ.get('SELF_DELETE', True)
 if SELF_DELETE == "True":
     SELF_DELETE = True
